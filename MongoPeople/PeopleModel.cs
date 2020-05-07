9952748f9d186a96b5c816058897bf0f8f0444bd
Justin Kloetzer <justin.kloetzer@gmail.com>
2020-05-07 11:21:32 -0500

    Added code

---
+++ MongoPeople/PeopleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoPeople
+{
+    class PeopleModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ MongoPeople/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MongoPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbName = "People";
+            var connString = "mongodb+srv://jkloetzer:Juke0ut1m!@cluster0-vdmjk.mongodb.net/test?retryWrites=true&w=majority";
+
+            var client = new MongoClient(connString + dbName);
+
+            Console.WriteLine("Welcome to MongoPeople");
+
+            Console.ReadKey();
+        }
+    }
+}
